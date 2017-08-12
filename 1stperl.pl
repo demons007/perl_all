035f0071a1541caa4109fc207eec677fa10d50db
demons007 <demons007@users.noreply.github.com>
2017-08-13 01:29:56 +0530

    Add files via upload
    print and \n

---
+++ 1stperl.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+# to comment use "#"
+
+print "Hello World\n";
+print 'Hello World\n';
+
+#\n --> new line escape sequence
+
+$my_var=200;
+
+print "\nValue is : $my_var\n";
+print 'Value is : $my_var\n';
